ac28a6378a8a8109707bd656004611953030f4c7
Rafaela Atanasio <rah.jd@hotmail.com>
2024-05-20 21:42:47 -0300

    Até issue 2.2

--- movie-challenge-angular/src/models/Movie.d.ts
+++ movie-challenge-angular/src/models/Movie.d.ts
@@ -1,6 +1,11 @@
+// definições de modelos de dados usados na api
+
 export type Movie = { // o que quero mostrar em tela / renderização
-    title: string;
-    poster: string;
-    realese_year: number;
     id: number;
+    title: string;
+    poster_path: string;
+    release_year: string;
+    overview: string;
+    vote_average: number;
+    genre_ids: number[];
 }
--- movie-challenge-angular/src/utils/transformers.ts
+++ movie-challenge-angular/src/utils/transformers.ts
@@ -1,5 +1,28 @@
-const formatMovie = () => { // Esta função será responsável pela transformação dos dados do filme da API para o modelo de negócios Movie.
+// funções de utilidade compartilhadas
 
+import { Movie } from '../models/Movie';
+
+type RawMovieData = { //dados brutos
+  id: number;
+  title: string;
+  poster_path: string;
+  release_date: string;
+  overview: string;
+  vote_average: number;
+  genre_ids: number[];
 }
+
+const formatMovie =  (rawData: RawMovieData): Movie => { // Esta função será responsável pela transformação dos dados do filme da API para o modelo de negócios Movie.
+// criando um dado bruto. Como converter um dado bruto em movie no formato js ou ts?
+return {
+    id: rawData.id,
+    title: rawData.title,
+    poster_path: rawData.poster_path,
+    release_year: rawData.release_date,
+    overview: rawData.overview,
+    vote_average: rawData.vote_average,
+    genre_ids: rawData.genre_ids,
+  };
+};
 
 export {formatMovie}
